921e6630980b42eb8ca95628cbd78b7921213894
Lucas Devers-Dore <lucas.devers-dore@etu.univ-orleans.fr>
2024-02-08 08:15:32 +0100

    test

--- Auteur.java
+++ Auteur.java
@@ -53,4 +53,47 @@ public class Auteur{
         String debut = "L'honorable : ";
         return debut.concat(this.nom);
     }
+    
+    public Style pointsFort(){
+    int ptsComedie=this.getQualiteComedie();
+    int ptsDrame=this.getQualiteDrame();
+    int ptsTragedie=this.getQualiteTragedie();
+    if ((ptsComedie>ptsDrame)&&(ptsComedie>ptsTragedie)){
+      return Style.COMÉDIE;
+    }
+    else if (ptsDrame>ptsComedie && ptsDrame>ptsTragedie){
+      return Style.DRAME;
+    }
+    else{
+      return Style.TRAGÉDIE;
+    }
+  }
+
+    public int getQualitéStyle(Style style){
+    switch (style) {
+      case COMÉDIE:
+        return this.getQualiteComedie();
+      case TRAGÉDIE:
+        return this.getQualiteTragedie();
+      case DRAME:
+        return this.getQualiteDrame();
+
+      default:
+        return -1;
+    }
+  }
+
+    public String citationStyle(Style s) {
+        if (Style.TRAGÉDIE==s){
+          return this.getCitationTragedie();
+        }
+        if (Style.DRAME==s){
+          return this.getCitationDrame();
+        }
+        else{
+          return this.getCitationComedie();
+        }
+  }
+
+
 }
